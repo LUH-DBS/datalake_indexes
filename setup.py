37a7dd5dbd2a33400c74bb6a601177c074f7001a
Jannis Becktepe <jannis@becktepe.de>
2022-12-27 15:57:16 +0100

    demo setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='datalake-indexes',
+    version='0.0.1',
+    author='Jannis Becktepe',
+    description='Multi-attribute and Order Indexes for Data Discovery',
+    url='https://github.com/LUH-DBS/datalake_indexes',
+    keywords='data discovery, data lake',
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'seaborn',
+        'tqdm',
+        'sklearn'
+        # TODO fill
+    ],
+    packages=find_packages(
+        exclude=('datasts', 'demonstration', 'fig', 'temp_data'),
+        include=('datalake_indexes')
+    )
+)
